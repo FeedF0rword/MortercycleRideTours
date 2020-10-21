25631d78eb2b7bc5d8c4ed8a521f83c0a8cc278f
FeedF0rword <72493404+FeedF0rword@users.noreply.github.com>
2020-10-21 19:17:29 +0530

    Add files via upload

---
+++ src/main/java/com/BCASKandy/MorterCycleTourPlanner/MorterCycleTourPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.BCASKandy.MorterCycleTourPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorterCycleTourPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorterCycleTourPlannerApplication.class, args);
+	}
+
+}
