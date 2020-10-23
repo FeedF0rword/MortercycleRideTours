1d6552f0403405ce817a0d6a025d9a5ccccb50b0
RoarshacH <smv.dilshan@gmail.com>
2020-10-23 06:33:36 +0530

    Modifying the User

--- src/main/java/com/BCASKandy/MorterCycleTourPlanner/controller/UserController.java
+++ src/main/java/com/BCASKandy/MorterCycleTourPlanner/controller/UserController.java
@@ -2,7 +2,10 @@ package com.BCASKandy.MorterCycleTourPlanner.controller;
 
 import com.BCASKandy.MorterCycleTourPlanner.model.User;
 import com.BCASKandy.MorterCycleTourPlanner.repository.UserRepository;
+import com.BCASKandy.MorterCycleTourPlanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
-
+    @GetMapping("/")
+    public void getAllUsers(Model model){
+        
+    }
 
 
 
--- src/main/java/com/BCASKandy/MorterCycleTourPlanner/service/UserFacade.java
+++ src/main/java/com/BCASKandy/MorterCycleTourPlanner/service/UserFacade.java
@@ -1,5 +1,10 @@
-package com.BCASKandy.MorterCycleTourPlanner.model;
+package com.BCASKandy.MorterCycleTourPlanner.service;
 
+
+import com.BCASKandy.MorterCycleTourPlanner.model.Role;
+import com.BCASKandy.MorterCycleTourPlanner.model.User;
+import com.BCASKandy.MorterCycleTourPlanner.model.UserInformation;
+import org.springframework.data.repository.Repository;
 
 import javax.naming.AuthenticationException;
 import java.util.List;
